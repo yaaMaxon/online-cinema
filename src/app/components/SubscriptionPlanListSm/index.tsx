656e99656168223fca2c21d8f2c605959c914ee1
Maksym <maks.sansei@gmail.com>
2025-01-30 14:58:33 +0200

    fixed img size in categoris and subscription items position

--- src/app/components/SubscriptionPlanListSm/index.tsx
+++ src/app/components/SubscriptionPlanListSm/index.tsx
@@ -13,21 +13,23 @@ const SubscriptionPlanListSm = () => {
 
   return (
     <div className="lg:hidden">
-      <ul className="flex bg-[#0F0F0F] rounded-lg border-[1px] border-[#262626] p-2 mb-5">
-        {["Basic", "Standard", "Premium"].map((plan, index) => (
-          <li
-            key={index}
-            className={`${
-              activePlan === plan
-                ? "bg-[#1F1F1F] rounded-md text-white"
-                : "text-default"
-            } text-sm font-medium py-3 px-5 cursor-pointer`}
-            onClick={() => handleActivePlan(plan)}
-          >
-            {plan}
-          </li>
-        ))}
-      </ul>
+      <div className="flex justify-center mb-5">
+        <ul className="flex bg-[#0F0F0F] rounded-lg border-[1px] border-[#262626] p-2">
+          {["Basic", "Standard", "Premium"].map((plan, index) => (
+            <li
+              key={index}
+              className={`${
+                activePlan === plan
+                  ? "bg-[#1F1F1F] rounded-md text-white"
+                  : "text-default"
+              } text-sm font-medium py-3 px-5 cursor-pointer`}
+              onClick={() => handleActivePlan(plan)}
+            >
+              {plan}
+            </li>
+          ))}
+        </ul>
+      </div>
       <ul className="grid grid-cols-2 gap-6 bg-[#0F0F0F] border border-[#262626] rounded-xl p-6">
         {planeConditions.length > 0 &&
           planeConditions.map((plan, index) => (
